d0f8f281021fd44a5ce6a73ad1464c6d3f737a0d
chepetski <met-79@,mail.ru>
2023-10-12 13:28:50 +0300

    new branch commit

--- main.c
+++ main.c
@@ -1,10 +1,6 @@
 #include<stdio.h>
 
 main(void){
-    printf("Hello World!!! \n");
-    printf("Hello Russia!!! \n");
-    printf("Hello my friend!!!!!!!! \n"); 
-    printf("Hello !!!!!!!! \n"); 
-    printf("Hello ! \n"); 
-     printf("Hello ! \n");    
+    printf("Good buy World!!! \n");
+
 }
